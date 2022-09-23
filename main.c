44fdb6968cce8c34056030d45d2de36b97018240
chandram13 <mschandr@uci.edu>
2022-09-22 17:12:21 -0700

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+// Marvish Chandra
+// Wiping a Dashboard, based on the wiper's rotation creating 8 semicircles
+
+#include <stdio.h>
+#include <math.h>
+
+int main void(n, w){
+    // largest possible area
+    ((3.14) * h) / 8 * sqrt((h * h) + 1);
+    // optimal area in a unit square
+    (3.14 / 2) * (2- sqrt(2) + 8 * (1 / (sqrt(2)) + 2 * n)^ 2 * (sqrt(2) + 2 + 2 * n)^ 2)
+    // largest possible area for the equation 0 < w < 1 for semicircle placement
+    (3.14 / 2) * (1 / (n + 1 / w - 1)^ 2 * (n + 1 / w)^ 2)
+}
